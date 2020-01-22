0bc12f1e5e58fb6e6eb3b71fa8a0d468e2e528f8
Bo Lingen <lingenbw@gmail.com>
2020-01-22 01:05:03 -0600

    init

---
+++ readme.md
@@ -0,0 +1,77 @@
+# @converge/state &middot; [![Version](https://flat.badgen.net/npm/v/@converge/state)](https://www.npmjs.com/package/@converge/state) [![License](https://flat.badgen.net/npm/license/@converge/state)](https://www.npmjs.com/package/@converge/state) [![JavaScript Standard Style](https://flat.badgen.net/badge/code%20style/standard/green)](https://standardjs.com)
+
+> Minimal state management library similar to `redux`, with `immer` built in.
+
+## installation
+
+```sh
+# using yarn:
+yarn add @converge/state
+
+# or npm:
+npm i @converge/state
+```
+
+## usage
+
+```js
+import { createStore } from '@converge/state'
+
+const store = createStore({ count: 0 }, {
+  plus: (by = 1) => state => { state.count += by },
+  minus: (by = 1) => state => { state.count -= by },
+  reset: () => () => ({ count: 0 })
+})
+
+const { plus, minus, reset } = store.getActions()
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState())
+})
+
+plus()
+// -> { count: 1 }
+
+plus(2)
+// -> { count: 3 }
+
+minus(2)
+// -> { count: 1 }
+
+reset()
+// -> { count: 0 }
+
+unsubscribe()
+
+const { state, actions } = store.useStore()
+
+console.log(state)
+// -> { count: 0 }
+
+actions.plus(10)
+
+console.log(state)
+// -> { count: 0 }
+
+console.log(store.getState())
+// -> { count: 10 }
+```
+
+## see also
+
+* [`redux`][redux] &ndash; predictable state container for JS apps
+* [`immer`][immer] &ndash; create the next immutable state by mutating the current one
+
+## contributing
+
+Search the [issues](https://github.com/converge/state) if you come
+across any trouble, open a new one if it hasn't been posted, or, if you're
+able, open a [pull request](https://help.github.com/articles/about-pull-requests/).
+Contributions of any kind are welcome in this project.
+
+## license
+
+MIT © [Bo Lingen / citycide](https://github.com/citycide)
+
+[redux]: https://github.com/reduxjs/redux
+[immer]: https://github.com/immerjs/immer
---
+++ src/index.ts
@@ -0,0 +1,96 @@
+import {
+  produce,
+  applyPatches,
+  isDraftable
+} from 'immer'
+
+import /* type */ { Draft, Patch } from 'immer'
+
+type SetState <S> =
+  (draft: Draft<S>) => void | S | PromiseLike<void | S>
+
+type ReplaceReturnType <T extends (...args: any[]) => any, U> =
+  (...args: Parameters<T>) => U
+
+type ReturnActions <S, A extends Actions<S>> = {
+  [K in keyof A]: ReplaceReturnType<A[K], void>
+}
+
+export interface Actions <S> {
+  [key: string]: (...payload: any[]) => SetState<S>
+}
+
+export interface StaticStore <S, A extends Actions<S>> {
+  state: Readonly<S>,
+  actions: ReturnActions<S, A>
+}
+
+export interface Store <S, A extends Actions<S>> {
+  useStore: () => StaticStore<S, A>
+  getState: () => Readonly<S>
+  getActions: () => ReturnActions<S, A>,
+  subscribe: (subscriber: () => any) => () => void
+}
+
+const isThenable = (value: unknown): value is PromiseLike<unknown> =>
+  typeof value === 'object' &&
+  typeof (value as any).then === 'function'
+
+export const createStore = <S, R extends Actions<S>> (
+  initialState: S,
+  reducers: R = {} as R
+): Store<S, R> => {
+  let currentState = initialState
+  let changes: Patch[] = []
+  let inverseChanges: Patch[] = []
+
+  const subscriptions: Set<() => any> = new Set()
+
+  const performUpdate = (state: S) => {
+    if (isDraftable(state)) {
+      const result = applyPatches(currentState, changes)
+      changes = []
+      inverseChanges = []
+      currentState = result
+    } else {
+      currentState = state
+    }
+
+    subscriptions.forEach(subscriber => subscriber())
+  }
+
+  const getAction = (key: string) => (...args: any[]) => {
+    const setState = reducers[key](...args)
+    const result = produce(
+      currentState,
+      setState,
+      (patches: Patch[], inversePatches: Patch[]) => {
+        changes.push(...patches)
+        inverseChanges.push(...inversePatches)
+      }
+    )
+
+    if (isThenable(result)) {
+      ;(result as PromiseLike<S>).then(performUpdate)
+    } else {
+      performUpdate(result as S)
+    }
+  }
+
+  const proxy: ReturnActions<S, R> = new Proxy(reducers, {
+    get: (_target, key, _desc) => getAction(key as string)
+  })
+
+  return {
+    getState: () => currentState ?? initialState,
+    getActions: () => proxy,
+    subscribe: subscriber => {
+      subscriptions.add(subscriber)
+      return () => subscriptions.delete(subscriber)
+    },
+    useStore: () => ({
+      state: currentState ?? initialState,
+      actions: proxy
+    })
+  }
+}
